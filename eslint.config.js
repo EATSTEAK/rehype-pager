0059d536e60038f8f86c9bedfa4a7c0dc2bcbd1a
EATSTEAK <me@eatsteak.dev>
2024-12-10 00:40:47 +0900

    lint: fix lint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import tsEslint from 'typescript-eslint';
 import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tsEslint.config(
-    eslint.configs.recommended,
-    tsEslint.configs.recommended,
-    prettierRecommended,
-)
+  eslint.configs.recommended,
+  tsEslint.configs.recommended,
+  prettierRecommended
+);
