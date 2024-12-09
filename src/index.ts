adb43290c7f9194156c946d667eaad1a402ec1d5
EATSTEAK <me@eatsteak.dev>
2024-12-10 00:40:57 +0900

    fix: return root element

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import type { Plugin } from "unified";
-import type { Element, Root } from "hast";
+import type { Plugin } from 'unified';
+import type { Element, Root } from 'hast';
 
 const createPagerSection = (children: Element[] = []): Element => {
   return {
-    type: "element",
-    tagName: "section",
+    type: 'element',
+    tagName: 'section',
     properties: {
-      className: ["page"],
+      className: ['page'],
     },
     children,
   };
@@ -14,15 +14,19 @@ const createPagerSection = (children: Element[] = []): Element => {
 
 const pager: Plugin<[], Root> = () => {
   return (tree: Root) => {
-    let pagedChildren = tree.children.reduce<Element[]>(
+    const pagedChildren = tree.children.reduce<Element[]>(
       (res, val) => {
-        if (val.type === "element" && val.tagName === "hr")
+        if (val.type === 'element' && val.tagName === 'hr')
           return [...res, createPagerSection()];
-        if (val.type !== "doctype") res[res.length - 1].children.push(val);
+        if (val.type !== 'doctype') res[res.length - 1].children.push(val);
         return res;
       },
-      [createPagerSection()],
+      [createPagerSection()]
     );
+    return {
+      ...tree,
+      children: pagedChildren
+    }
   };
 };
 
